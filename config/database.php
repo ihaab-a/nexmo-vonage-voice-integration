d64c8097c2953d7ceb911358cce8176a08ea9977
Ihaab <ihaabali879@gmail.com>
2022-10-07 11:03:18 +0800

    updated db config, arranged dependencies

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 define('DB_HOST', 'localhost');
-define('DB_DATABASE', '045starpromo_prank');
+define('DB_DATABASE', 'starpromo_prank');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 
